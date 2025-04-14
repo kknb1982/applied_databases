4e035c9b0da985fe93f4392d9519ac5de5dcd639
kknb1982 <kirstin.barnett@gmail.com>
2025-04-14 11:41:30 +0100

    menu option 3 added

--- G000425638/PythonApp/main.py
+++ G000425638/PythonApp/main.py
@@ -1,16 +1,18 @@
 from menu import menu
 from menu1_directors import get_directors_by_name
 from menu2_actor_by_dob import get_birth_month, get_actor_by_month
-#from menu3_new_actor import add_new_actor
+from menu3_new_actor import check_actor_ID
 import mysql.connector as msql
 
 def menu():
     options = "MENU \n 1 - View Directors & Film \n 2 - View Actors by Month of Birth \n 3 - Add New Actor \n 4 - View Married Actors \n 5 - Add Actor Marriage \n 6. View Studios \n x - Exit Application"
     print(options)
     choice = input("Choose a menu option: ")
+    
     if choice == "1":
         get_directors_by_name()
         menu()
+    
     elif choice == "2":
         month_num = get_birth_month()
         if month_num:
@@ -18,6 +20,11 @@ def menu():
         else:
             print("Invalid month input. Please try again.")
         menu()
+    
+    elif choice == "3":
+        check_actor_ID()
+        menu()
+
 if __name__ == "__main__":
     menu()
 
@@ -26,8 +33,7 @@ if __name__ == "__main__":
 
     
     
-elif choice == "3":
-    add_new_actor()
+
 
 elif choice == "4":
     view_married_actors()
