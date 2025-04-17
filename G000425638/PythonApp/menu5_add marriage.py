4aec0d892e6d0c1ab4b682300c7f7956a8826dc5
kknb1982 <kirstin.barnett@gmail.com>
2025-04-17 17:53:04 +0100

    Added code to check for divorce

--- G000425638/PythonApp/menu5_add marriage.py
+++ G000425638/PythonApp/menu5_add marriage.py
@@ -6,8 +6,28 @@ password = "neo4jneo4j"
 driver = GraphDatabase.driver(uri, auth=(username, password))
 
 def check_actor_exists(actor_id):
-    result = session.execute_read(lambda tx: tx.run("MATCH (a:Actor {ActorID: $id}) RETURN a", id=actor_id).single())
-    return result is not None
+    with driver.session() as session:
+        result = session.execute_read(lambda tx: tx.run(
+            "MATCH (a:Actor {ActorID: $id}) RETURN a", id=actor_id).single())
+        return result is not None
+    
+def is_actor_married(actor_id):
+    with driver.session() as session:
+        result = session.execute_read(lambda tx: tx.run(
+            """
+            MATCH (a:Actor {ActorID: $id})-[:MARRIED_TO]-(:Actor)
+            RETURN a
+            """, id=actor_id).single())
+        return result is not None
+
+def was_divorced(actor1id, actor2id):
+    with driver.session() as session:
+        result = session.execute_read(lambda tx: tx.run(
+            """
+            MATCH (a:Actor {ActorID: $id1})-[:DIVORCED_FROM]-(b:Actor)
+            RETURN a
+            """, id=actor_id).single())
+        return result is not None
     
 def get_valid_actor(prompt_text):
     while True:
@@ -29,8 +49,22 @@ def create_marriage(tx, actor1id, actor2id):
             """, id1=actor1id, id2=actor2id))
     print(f"Actor {actor1id} is now married to Actor {actor2id}.")    
 
-actor1id = input("Enter Actor 1 ID: ")
-actor2id = input("Enter Actor 2 ID: ")
+actor1id = get_valid_actor("Enter Actor 1 ID: ")
+actor2id = get_valid_actor("Enter Actor 2 ID: ")
+
+married1 = is_actor_married(actor1id)
+married2 = is_actor_married(actor2id)
+
+divorced1 = was_divorced(actor1id)
+divorced2 = was_divorced(actor2id)
+
+errors = []
+
+if married1 and not divorced1:
+    errors.append(f"Actor {actor1id} is already married and hasn't been divorced.")
+if married2 and not divorced2:
+    errors.append(f"Actor {actor2id} is already married and hasn't been divorced.")
+
 
 create_marriage(actor1id, actor2id)
 driver.close()
