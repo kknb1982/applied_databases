02463f480d727d0a87fbce7d13f5aaa8dadaf24f
kknb1982 <kirstin.barnett@gmail.com>
2025-04-17 18:08:28 +0100

    updated option 6

--- G000425638/PythonApp/menu.py
+++ G000425638/PythonApp/menu.py
@@ -1,6 +1,6 @@
 # Create the options menu
 def menu():
-    options = "MENU \n 1 - View Directors & Film \n 2 - View Actors by Month of Birth \n 3 - Add New Actor \n 4 - View Married Actors \n 5 - Add Actor Marriage \n 6. View Studios \n x - Exit Application"
+    options = "MENU \n 1 - View Directors & Film \n 2 - View Actors by Month of Birth \n 3 - Add New Actor \n 4 - View Married Actors \n 5 - Add Actor Marriage \n 6 - View Studios \n x - Exit Application"
     print(options)
     choice = input("Choose a menu option: ")
 
