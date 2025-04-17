4230620be0fb3cec27c1adb95e717a120d4747ab
kknb1982 <kirstin.barnett@gmail.com>
2025-04-17 16:27:53 +0100

    tidied code

--- G000425638/PythonApp/menu4_married.py
+++ G000425638/PythonApp/menu4_married.py
@@ -1,16 +1,9 @@
 from neo4j import GraphDatabase
+
 uri = "neo4j://localhost:7687"
 username = "neo4j"
 password = "neo4jneo4j"
 driver = GraphDatabase.driver(uri, auth=(username, password))
-
-
-from neo4j import GraphDatabase
-
-# Neo4j connection settings
-uri = "bolt://localhost:7687"
-username = "neo4j"
-password = "your_password"  # Replace with your actual Neo4j password
 
 def check_marriage(actor_id):
     driver = GraphDatabase.driver(uri, auth=(username, password))
