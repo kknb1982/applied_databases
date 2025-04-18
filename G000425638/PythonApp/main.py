c5859425a93538d0a302c03059dfc693a1d120bf
kknb1982 <kirstin.barnett@gmail.com>
2025-04-18 10:44:27 +0100

    cleaned code to menu

--- G000425638/PythonApp/main.py
+++ G000425638/PythonApp/main.py
@@ -15,56 +15,7 @@ from neo4j import GraphDatabase
 con = None
 driver = None
 
-if choice == "1":
-        get_directors_by_name()
-        menu()
-elif choice == "2":
-        month_num = get_birth_month()
-        if month_num:
-            get_actor_by_month(month_num)
-        else:
-            print("Invalid month input. Please try again.")
-        menu()
-    
-elif choice == "3":
-        add_actor()
-        menu()
-        
-elif choice == "4":
-        check_marriage()
-        menu()
-        
-elif choice == "5": 
-    user_input = input("Enter the Actor ID to check for marriages: ")
-    check_marriage(user_input)
-    
-        actor1id = get_valid_actor("Enter Actor 1 ID: ")
-        actor2id = get_valid_actor("Enter Actor 2 ID: ")
-
-        married1 = is_actor_married(actor1id)
-        married2 = is_actor_married(actor2id)
-
-        divorced1 = was_divorced(actor1id)
-        divorced2 = was_divorced(actor2id)
-
-        errors = []
-
-        if married1 and not divorced1:
-            errors.append(f"Actor {actor1id} is already married and hasn't been divorced.")
-        if married2 and not divorced2:
-            errors.append(f"Actor {actor2id} is already married and hasn't been divorced.")
-            menu()
-        
-elif choice == "6":
-        get_studios()
-        menu()
-        
-elif choice == "x":
-        print("Exiting application...") 
-else:
-        print(f"Invalid choice. Please try again.")
-        menu()
-    
+  
 if __name__ == "__main__":
     menu()
 
