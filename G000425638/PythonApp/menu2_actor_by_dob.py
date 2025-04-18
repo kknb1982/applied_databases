bcbf4584c9ff8d69cea1ac33d9f588b07f01738d
kknb1982 <kirstin.barnett@gmail.com>
2025-04-18 17:24:17 +0100

    cleaned code

--- G000425638/PythonApp/menu2_actor_by_dob.py
+++ G000425638/PythonApp/menu2_actor_by_dob.py
@@ -5,14 +5,3 @@ con = None
 def connect():
 	global con
 	con = pymysql.connect(host='localhost', database='appdbproj', user='root', password='', cusorclass=pymysql.cursors.DictCursor) 
-
-# Get birth month function - This checks the input of the month against a dictioonary of months and returns the month number.
-
-                 
-    except msql.Error as err:
-        print(f"Error: {err}")
-    
-    finally:
-        cursor.close()
-        con.close()
-        
