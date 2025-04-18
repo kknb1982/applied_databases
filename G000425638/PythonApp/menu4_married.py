faeca06b227b17c605eea8a3cf17f3aaebcdebb4
kknb1982 <kirstin.barnett@gmail.com>
2025-04-18 15:30:44 +0100

    cleaned code

--- G000425638/PythonApp/menu4_married.py
+++ G000425638/PythonApp/menu4_married.py
@@ -5,26 +5,12 @@ username = "neo4j"
 password = "neo4jneo4j"
 driver = GraphDatabase.driver(uri, auth=(username, password))
 
-def check_marriage(actor_id):
-    driver = GraphDatabase.driver(uri, auth=(username, password))
-    with driver.session() as session:
-        result = session.execute_read(find_marriage, actor_id)
-
-        if result:
-            print("These actors are married:")
-            for pair in result:
-                print(f"Actor ID: {pair['id1']} - Name: {pair['name1']}")
-                print(f"Actor ID: {pair['id2']} - Name: {pair['name2']}")
-        else:
-            print("No marriage found for that actor ID.")
-
-    driver.close()
-
-def find_marriage(tx, actor_id):
+def find_spouse(actor_id):
     query = """
-    MATCH (a:Actor {ActorID: $actor_id})-[r:MARRIED_TO]-(partner:Actor)
-    RETURN a.ActorID AS id1, a.name AS name1,
-           partner.ActorID AS id2, partner.name AS name2
+    MATCH (a:Actor {ActorID: $actorId})-[:MARRIED_TO]-(spouse:Actor)
+    RETURN a.ActorID AS ActorID, a.ActorName AS ActorName, spouse.ActorID AS SpouseID, spouse.ActorName AS SpouseName
     """
-    return list(tx.run(query, actor_id=int(actor_id)))
+    with driver.session() as session:
+        result = session.run(query, actorId=int(actor_id))
+        return list(result)
 
